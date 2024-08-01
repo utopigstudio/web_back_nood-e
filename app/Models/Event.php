ede8bf98ad6d6375834d5a5b528bc142e7a89d1e
Lau R <laurarodriguez@utopigstudio.com>
2024-08-01 20:00:39 +0200

    adapt events start and end

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -13,8 +13,8 @@ class Event extends Model
     protected $fillable = [
         'title',
         'description',
-        'start_date',
-        'end_date',
+        'start',
+        'end',
         'room',
         'price',
         'image'
