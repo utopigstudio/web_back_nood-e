72465afa7dd071baa470136c12544e9a62ca8823
LlorensUtopig <llorens@utopigstudio.com>
2024-08-27 13:25:22 +0200

    return array instade of object

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all()->sortBy('name');
+        $users = User::with('organization', 'roles')->orderBy('name')->get()->values();
         return response()->json($users);
     }
     
