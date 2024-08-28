5a38d23359a85e8482c8ecd73bc1c308ca1c790d
Maria Vilaró <maria@utopigstudio.com>
2024-08-28 18:57:16 +0200

    cleanup intervention installation

--- config/app.php
+++ config/app.php
@@ -122,9 +122,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    'aliases' => [
-        'Image' => Intervention\Image\Laravel\Facades\Image::class,
-    ],
-
 ];
