8d093bb4998a49a2018f962b19cc9846f6bc92bc
Maria Vilaró <maria@utopigstudio.com>
2024-09-03 20:34:00 +0200

    fix password reset test

--- tests/Feature/Auth/PasswordResetTest.php
+++ tests/Feature/Auth/PasswordResetTest.php
@@ -2,73 +2,52 @@
 
 namespace Tests\Feature\Auth;
 
-use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
+use Tests\Support\Authentication;
 use Tests\TestCase;
-use Tymon\JWTAuth\Facades\JWTAuth;
 
 class PasswordResetTest extends TestCase
 {
-    use RefreshDatabase;
-
-    private function createAuthUser (): array
-    {
-        $user = User::factory()->create();
-        $token = JWTAuth::fromUser($user);
-
-        return ['user' => $user, 'token' => $token];
-    }
+    use RefreshDatabase, Authentication;
 
     public function test_reset_password_link_can_be_requested(): void
     {
         Notification::fake();
 
-        $authData = $this->createAuthUser();
-        $user = $authData['user'];
-        $token = $authData['token'];
+        $this->post('/api/v1/auth/forgot-password',
+            ['email' => $this->user->email]
+        );
 
-        $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token,
-        ]);
-
-        $this->actingAs($user);
-
-        $this->post('/api/v1/auth/forgot-password', ['email' => $user->email]);
-
-        Notification::assertSentTo($user, ResetPassword::class);
+        Notification::assertSentTo($this->user, ResetPassword::class);
     }
 
     public function test_password_can_be_reset_with_valid_token(): void
     {
         Notification::fake();
 
-        $authData = $this->createAuthUser();
-        $user = $authData['user'];
-        $token = $authData['token'];
+        $this->post('/api/v1/auth/forgot-password', ['email' => $this->user->email]);
 
-        $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token,
-        ]);
-
-        $this->actingAs($user);
-
-        $this->post('/api/v1/auth/forgot-password', ['email' => $user->email]);
-
-        Notification::assertSentTo($user, ResetPassword::class, function (object $notification) use ($user) {
-            $response = $this->post('/api/v1/auth/reset-password', [
+        Notification::assertSentTo($this->user, ResetPassword::class, function (object $notification) {
+            $this->post('/api/v1/auth/reset-password', [
                 'token' => $notification->token,
-                'email' => $user->email,
+                'email' => $this->user->email,
                 'password' => 'new password',
                 'password_confirmation' => 'new password',
-            ]);
-
-            $response
-                ->assertSessionHasNoErrors()
-                ->assertStatus(200);
+            ])->assertStatus(200);
 
             return true;
         });
+    }
+
+    public function test_password_cannot_be_reset_with_invalid_token(): void
+    {
+        $this->post('/api/v1/auth/reset-password', [
+            'token' => 'invalid-token',
+            'email' => $this->user->email,
+            'password' => 'new password',
+            'password_confirmation' => 'new password',
+        ])->assertStatus(302);
     }
 }
