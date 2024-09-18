9b35f97da781cf580c21a65649e3d3df8fa8cc3b
Maria Vilaró <maria@utopigstudio.com>
2024-09-18 13:10:01 +0200

    clean up unused code until the end of the world

--- app/Helpers/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-function jsonResponse($data = [], $status = 200, $message = 'OK', $errors = []): \Illuminate\Http\JsonResponse
-{
-    return response()->json(compact('data', 'status', 'message', 'errors'), $status);
-}
