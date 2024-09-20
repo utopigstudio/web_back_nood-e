22dcab21d5662753c8a0f907c370464e8f26edfe
Jose Manuel <jose@utopigstudio.com>
2024-09-20 13:54:14 +0200

    Fix migration for refresh

--- database/migrations/2024_07_19_163619_create_organizations_table.php
+++ database/migrations/2024_07_19_163619_create_organizations_table.php
@@ -24,6 +24,10 @@ return new class extends Migration
 
     public function down(): void
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['organization_id']);
+        });
+
         Schema::dropIfExists('organizations');
     }
 };
